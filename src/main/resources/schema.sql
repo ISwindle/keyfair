7312863e147a1aa02bb59ea2149fb411dc0c2708
iswindle <isaac.swindle@gmail.com>
2021-09-18 09:57:05 -0500

    Added DB schema and repository pattern

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,31 @@
-DROP TABLE IF EXISTS TBL_EMPLOYEES;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Restaurants;
+DROP TABLE IF EXISTS Deals;
 
-CREATE TABLE TBL_EMPLOYEES (
+CREATE TABLE Users (
                                id INT AUTO_INCREMENT  PRIMARY KEY,
-                               first_name VARCHAR(250) NOT NULL,
-                               last_name VARCHAR(250) NOT NULL,
-                               email VARCHAR(250) DEFAULT NULL
+                               user_name VARCHAR(20) NOT NULL,
+                               password VARCHAR(200) NOT NULL,
+                               birthdate DATE
 );
+
+CREATE TABLE Restaurants (
+                       id INT AUTO_INCREMENT  PRIMARY KEY,
+                       name VARCHAR(250) NOT NULL,
+                       latitude Decimal(8,6) NOT NULL,
+                       longitude Decimal(9,6) NOT NULL
+);
+
+CREATE TABLE Deals (
+                       id INT AUTO_INCREMENT  PRIMARY KEY,
+                       restaurant_id INT NOT NULL,
+                       start_date DATE,
+                       end_date DATE,
+                       start_hour INT,
+                       end_hour INT,
+                       discount_code VARCHAR(250),
+                       description VARCHAR(2000),
+                       CONSTRAINT FK_RestaurantID FOREIGN KEY (restaurant_id)
+                       REFERENCES Restaurants(id)
+);
+
